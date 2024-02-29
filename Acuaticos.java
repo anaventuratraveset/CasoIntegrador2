e44e770c3d119b13e5fae3cfc8411ddbb4719fe4
Ana Ventura-Traveset <a.venturatraveset@usp.ceu.es>
2024-02-29 14:08:39 +0100

    Primer commit

---
+++ Acuaticos.java
@@ -0,0 +1,12 @@
+public class Acuaticos {
+
+    public Acuaticos (){
+        //Al tener el public no se puede usar sin antes crear un objeto de tipo Acuaticos
+
+    }
+
+    public static void main(String[] args) {
+        Acuaticos miAcuatico = new Acuaticos();
+
+    }
+}
